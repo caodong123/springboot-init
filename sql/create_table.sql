80dafc96a8e173c052c9cd9066251afdb6d7aa9b
XiaoC <2763645165@qq.com>
2024-10-23 23:07:32 +0800

    初始化配置，编写sql，引入mp生成代码

---
+++ sql/create_table.sql
@@ -0,0 +1,22 @@
+# 数据库初始化
+
+-- 创建库
+create database if not exists init_db;
+
+-- 切换数据库
+use init_db;
+
+-- 用户表
+create table if not exists user
+(
+    id              bigint auto_increment                     comment 'id主键' primary key,
+    userAccount     varchar(256)                              not null comment '用户账号',
+    userPassword    varchar(256)                              not null comment '用户密码',
+    userName        varchar(256)                              null     comment '用户昵称',
+    userAvatar      varchar(1024)                             null     comment '用户头像',
+    userProfile     varchar(512)                              null     comment '用户简介',
+    userRole        varchar(256) default 'user'               not null comment '用户角色   user/admin/ban',
+    createTime      datetime default CURRENT_TIMESTAMP        not null comment '创建时间',
+    updateTime      datetime default CURRENT_TIMESTAMP        not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete        tinyint  default 0                        not null comment '逻辑删除 0未删除 1已删除'
+) comment '用户表' collate = utf8mb4_unicode_ci;
