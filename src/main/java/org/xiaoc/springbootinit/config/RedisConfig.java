2b8f74b2ff4826cee7e0a58dc0f88b1f2e8bdda6
XiaoC <2763645165@qq.com>
2024-10-28 15:44:05 +0800

    项目引入redis,并配置template以及Redisson

---
+++ src/main/java/org/xiaoc/springbootinit/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package org.xiaoc.springbootinit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
---
+++ src/test/java/org/xiaoc/springbootinit/redis/RedisTest.java
@@ -0,0 +1,25 @@
+package org.xiaoc.springbootinit.redis;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Resource
+    private StringRedisTemplate redisTemplate;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Test
+    public void testRedis(){
+        redisTemplate.opsForValue().set("name","xiaoc");
+        System.out.println(redisTemplate.opsForValue().get("name"));
+    }
+
+}
